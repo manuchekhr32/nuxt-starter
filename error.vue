15e02092b040da74b40cededfaaaf089c3109367
Manuchehr <manuchekhr.3232@gmail.com>
2024-02-10 11:42:29 +0500

    feat: add tailwindcss

---
+++ error.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+interface Props {
+  error: NuxtError
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <pre>{{ error }}</pre>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="bg-green-100">
     <nav>
       <ul>
         <li>
