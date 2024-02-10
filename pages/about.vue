1ea14f68335b219e3d9831abee4d1b473da0e125
Manuchehr <manuchekhr.3232@gmail.com>
2024-02-10 11:29:54 +0500

    feat: add pinia store

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+import { useMainStore } from '~/stores/main'
+
+const mainStore = useMainStore()
+</script>
+
 <template>
   <div>
     <h1>I'm about page</h1>
+    <button @click="mainStore.increase()">
+      Count: {{ mainStore.getCount }}
+    </button>
   </div>
 </template>
---
+++ stores/main.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useMainStore = defineStore('main', {
+  state: () => ({
+    count: 0,
+  }),
+  getters: {
+    getCount: state => state.count,
+  },
+  actions: {
+    increase() {
+      this.count++
+    },
+  },
+})
