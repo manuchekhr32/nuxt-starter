f8f0d78f70965325e565a1fa19913155c65b1a2a
Manuchehr <manuchekhr.3232@gmail.com>
2024-07-05 12:14:44 +0500

    fix: minor erros & warnings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    './components/**/*.{js,vue,ts}',
-    './composables/**/*.{js,ts}',
-    './assets/**/*.{css}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './app.vue',
-    './error.vue',
+    "./components/**/*.{js,vue,ts}",
+    "./composables/**/*.{js,ts}",
+    "./assets/**/*.css",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
