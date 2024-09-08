516d8dd6c421cc8e8d8d5716566b704f61783349
Manuchehr <manuchekhr.3232@gmail.com>
2024-09-08 10:27:35 +0500

    feat: update vue 3.5 & other packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,12 @@ import eslintPlugin from "vite-plugin-eslint";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   vite: {
     plugins: [eslintPlugin()],
   },
+
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxt/eslint"],
   eslint: {},
+  compatibilityDate: "2024-09-08",
 });
